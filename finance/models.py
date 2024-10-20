c0c5c9dc4fdfb159fa05d5d36ff2ad5695524ae8
MenegussiT <felipe.menegussi.faria@gmail.com>
2024-10-20 15:15:18 -0300

    Criação de models

--- finance/models.py
+++ finance/models.py
@@ -1,3 +1,52 @@
 from django.db import models
 
-# Create your models here.
+
+class user (models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+    surname = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100)
+    password = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+class expense_category (models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return self.name
+
+class expense (models.Model):
+    id = models.AutoField(primary_key=True)
+    email = models.ForeignKey(user, on_delete=models.CASCADE)
+    category = models.ForeignKey(expense_category, on_delete=models.CASCADE)
+    description = models.CharField(max_length=180)
+    value = models.DecimalField(max_digits=10, decimal_places=2)
+    expense_date = models.DateField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.name
+    
+
+class revenue (models.Model):
+    id = models.AutoField(primary_key=True)
+    email = models.ForeignKey(user, on_delete=models.CASCADE)
+    wage = models.DecimalField(max_digits=10, decimal_places=2)
+    varible_gain = models.DecimalField(max_digits=10, decimal_places=2)
+    
+    def __str__(self):
+        return self.name
+    
+
+class revenue_category (models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return self.name
+    
